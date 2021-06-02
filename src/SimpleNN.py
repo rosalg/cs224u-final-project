70a7a07b3f7c574e91793eedcd1cb9028d39a201
Lucas Pauker <lucas.pauker@gmail.com>
2021-06-01 20:39:21 -0500

    bug fix

--- src/SimpleNN.py
+++ src/SimpleNN.py
@@ -5,18 +5,24 @@ from sklearn.neural_network import MLPClassifier
 class SimpleNN(Model):
     def __init__(self):
         self.vectorizer = TfidfVectorizer()
-        self.clf = MLPClassifier(hidden_layer_sizes=(50, 10, 2), solver='adam', tol=1e-5)
+        self.clf = MLPClassifier(hidden_layer_sizes=(50, 10, 2), solver='adam', tol=1e-5)  # , max_iter=1
 
-    def train(self, df : pd.DataFrame):
+    def transform(self, df : pd.DataFrame, test=False):
         corpus = df["Text"]
-        tfidf = TfidfVectorizer().fit_transform(corpus).toarray()
+        if test:
+            tfidf = self.vectorizer.transform(corpus).toarray()
+        else:
+            tfidf = self.vectorizer.fit_transform(corpus).toarray()
         punc = np.reshape(list(df["Punctuation"]), (len(df), -1))
         X = np.concatenate((tfidf, punc), axis=1)
+        return X
+
+    def train(self, df : pd.DataFrame):
+        X = self.transform(df)
         self.clf = self.clf.fit(X, df["Vote"])
         print("nn fitted")
 
     def predict_votes(self, df : pd.DataFrame):
-        new_corpus = df["Text"]
-        new_X = self.vectorizer.transform(new_corpus)
+        new_X = self.transform(df, test=True)
         predicted = self.clf.predict(new_X)
         return predicted
